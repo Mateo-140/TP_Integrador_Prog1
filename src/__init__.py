00b329bec7fc41d4d0d9509b9b0aa75ddf2ec450
Braian martinez <braianmartinez96@gmail.com>
2026-06-12 10:09:37 -0300

    fix: corregir __init__.py, eliminar imports circulares

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,2 @@
 # Paquete src - Sistema de gestión de países
-from src.funciones import (
-    cargar_csv,
-    guardar_csv,
-    mostrar_paises,
-    buscar_pais,
-    filtrar_continente,
-    filtrar_poblacion,
-    filtrar_superficie,
-    ordenar_nombre,
-    ordenar_poblacion,
-    ordenar_superficie
-)
-from src.validaciones import agregar_pais, actualizar_pais
-from src.estadisticas import mostrar_estadisticas
+
